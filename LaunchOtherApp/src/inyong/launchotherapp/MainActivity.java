24691a4b991621292a9d7af1dcfec932640b2d46
inyong1 <always4free@gmail.com>
2013-09-27 14:54:30 +0700

    tambah pesan dialog error folder data tidak ditemukan

--- LaunchOtherApp/src/inyong/launchotherapp/MainActivity.java
+++ LaunchOtherApp/src/inyong/launchotherapp/MainActivity.java
@@ -10,8 +10,8 @@ import android.content.*;
 public class MainActivity extends Activity implements OnClickListener
 {
 
-//buat objek Button/tombol (ane namai tombolMasukSetting)
-Button tombolMasukSetting;
+//buat objek Button/tombol (ane namai tombolMasukSetting dan tombolMasukFireTools)
+	Button tombolMasukSetting, tombolMasukFireTools;
 
 
     /** Called when the activity is first created. */
@@ -22,21 +22,65 @@ Button tombolMasukSetting;
 		// adalah menampilkan xml layout utama (res/layout/main.xml) layout utama
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-		
+
 		// inisialisasi tombol
-		tombolMasukSetting=(Button) findViewById(R.id.tombol_masuk_setting);
+		tombolMasukSetting = (Button) findViewById(R.id.id_tombol_masuk_setting);
+		// pasang clicklistener supaya bisa diclick
 		tombolMasukSetting.setOnClickListener(this);
-	
+
+		tombolMasukFireTools = (Button) findViewById(R.id.id_tombol_masuk_firetool);
+		tombolMasukFireTools.setOnClickListener(this);
     }
 	//method untuk mendeteksi saat ada yg diclick
-	public void onClick(View v){
-		masukSetting();
+	public void onClick(View v)
+	{
+		// fungsi switch untuk perpindah atara smua kemungkinan sampai didapati salah satu yg dicari
+		switch (v.getId())
+		{
+			case R.id.id_tombol_masuk_setting:
+				//jika id tombol yg diklik adlh id tombol masuk seting, maka jalankan fungsi masuk seting
+				// jika idnya bukan itu, lanjut ke fungsi switch berikutnya
+				jalankanAplikasiLain("com.android.settings");
+				// stop fungsi switch saat salah satu id terbaca
+				break;
+
+			case R.id.id_tombol_masuk_firetool:
+				jalankanAplikasiLain("inyong.xt530.tools");
+				break;
+
+			default: 
+				break;
+		}
 	}
-	
-	
-	//method masuk setting
-	public void masukSetting(){
-		Intent i = getPackageManager().getLaunchIntentForPackage("com.android.settings");
-		startActivity(i);
+
+
+	//method untuk menjalankan aplikasi lain
+	// dengan parameter nama folder data /packagename dari app yg mau dijalankan
+	public void jalankanAplikasiLain(String namaFolderData)
+	{
+		Intent i = getPackageManager().getLaunchIntentForPackage(namaFolderData);
+		if (i != null) // jika i (intent) tidak null (tidak kosong)
+		// berarti aplikasi dgn namafolderdata dimaksud ditemukan
+		{
+			// maka jalankan app tsb.
+			startActivity(i);
+		}
+		else
+		{ 
+			// sebaliknya aplikasi gak ada
+			// kita akan coba tampilin dialog ke user
+			// memberitahukan bahwa app dgn nama folder data dimaksud gak ditemukan
+			dialogErrorFolderTidakDitemukan(namaFolderData);
+		}
+	}
+
+
+	//code dialog error ditulis dalam method berbeda agar tdk terlalu ruwet
+	public void dialogErrorFolderTidakDitemukan(String namaFolder)
+	{
+		AlertDialog.Builder d = new AlertDialog.Builder(this);
+		d.setMessage("Ups...\n Inyong tidak menemukan aplikasi dengan folder data \n\"" + namaFolder+"\"");
+		d.setCancelable(true); // agar dialog bisa ditutup dengn tombol back
+		d.show(); // perintah agar dialog tampil
 	}
 }
